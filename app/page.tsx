a1e69f097fdaecee507dc2d3d42f5ae40a7a95b5
KuldeepJha5176 <ranacjha@gmail.com>
2025-04-07 13:44:43 +0530

    added clerk and shadcn init

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import {
+  
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 export default function Home() {
   return (
    <div>
-    hi rana
+    <SignedOut>
+              <SignInButton mode="modal"/>
+              <SignUpButton />
+             
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
    </div>
   );
 }
